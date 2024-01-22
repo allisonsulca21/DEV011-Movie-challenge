d1622e4c78bb374a821d79974a649dc30ca9fab3
Allison Sulca <asulca2112@gmail.com>
2024-01-22 18:50:33 -0500

    Renderizado inicial de peliculas (póster, título y año)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
